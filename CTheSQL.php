c0007e45c2a2d9ec5c532e2a8d25c9c269602ea5
Evan Frohlich <evan108108@gmail.com>
2013-03-28 18:41:54 -0300

    Update CTheSQL.php

--- CTheSQL.php
+++ CTheSQL.php
@@ -1,23 +1,23 @@
 <?php
-  cclass CTheSQL extends CActiveRecordBehavior
-  {
-    public function getSql($dataProvider=null, $tableAlias='t')
-    {
-      if(empty($dataProvider)) $dataProvider = $this->Owner->search();
-      $dataProvider->criteria->mergeWith($this->Owner->defaultScope());
-      $schema = Yii::app()->db->schema;
-      $cbuilder = new CDbCommandBuilder($schema);
-      $sqlCommand = $cbuilder->createFindCommand($this->Owner->tableName(), $dataProvider->criteria,$tableAlias);
-      $sql = $sqlCommand->text;
+	class CTheSQL extends CActiveRecordBehavior
+	{
+		public function getSql($dataProvider=null, $tableAlias='t')
+		{
+			if(empty($dataProvider)) $dataProvider = $this->Owner->search();
+			$dataProvider->criteria->mergeWith($this->Owner->defaultScope());
+			$schema = Yii::app()->db->schema;
+			$cbuilder = new CDbCommandBuilder($schema);
+			$sqlCommand = $cbuilder->createFindCommand($this->Owner->tableName(), $dataProvider->criteria,$tableAlias);
+			$sql = $sqlCommand->text;
 
-    //we need to reverse sort the params array so the str_replace will work correctly for keys with same prefix + higher index
-	  //eg:  :ycp0 .....:ycp01 
-	  $params = $dataProvider->criteria->params;
-	  krsort($params);
+			//we need to reverse sort the params array so the str_replace will work correctly for keys with the same prefix + higher index
+			//eg:  :ycp0 .....:ycp01 
+			$params = $dataProvider->criteria->params;
+			krsort($params);
 	
-      foreach($params as $key=>$value) {
-        $sql = str_replace($key, "'" . addslashes($value) . "'", $sql);
-	    }	
-      return $sql;
-    }
-  }
+			foreach($params as $key=>$value) {
+				$sql = str_replace($key, "'" . addslashes($value) . "'", $sql);
+			}	
+			return $sql;
+		}
+	}
